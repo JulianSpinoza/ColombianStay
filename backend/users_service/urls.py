25c08c1d5817714b5d3ad6ae2c5a27e961c3d5ac
JulianSpinoza <juliancamilosimpson@gmail.com>
2025-11-20 17:19:31 -0500

    Testing first functional service DRF

--- backend/users_service/urls.py
+++ backend/users_service/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+#from django.urls import path
 
-urlpatterns = [
+#urlpatterns = [
     # endpoints
-]
+#]
+
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
+
+urlpatterns = router.urls
