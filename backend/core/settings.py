3627184d43b67b76f835ed48db1851c31b2e8c95
JulianSpinoza <juliancamilosimpson@gmail.com>
2025-11-20 17:43:21 -0500

    CORS configuration on backend Django

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third-party apps
-    "rest_framework",
+    "rest_framework", # To do petitions
+    "corsheaders", # To accept petitions from the frontend
 
     # My services (apps)
     'users_service',
@@ -62,6 +63,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    # Check petitions for other domains
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,6 +73,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", # React domain (or 3000)
 ]
 
 ROOT_URLCONF = 'core.urls'
