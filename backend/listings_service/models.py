df8dc5d64abd7272f4853af58b676f091ad3d70e
Julian Espinosa <jucespinosam@udistrital.edu.co>
2026-03-27 23:13:40 -0500

    (backend): Model of ListingImage created. - Add a serializer to the ListingSerializer. - Add it volume where is the media is saved. - Add media url to access to it from the backend. - Make migration about the model and mockListingImages.

--- backend/listings_service/models.py
+++ backend/listings_service/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from django.db.models import Q
 
+from .utils import create_thumbnail
 class Region(models.Model):
   
     regionid = models.AutoField(primary_key=True)
@@ -43,3 +46,39 @@ class Listing(models.Model):
 
     class Meta:
         db_table = 'accomodation'
+
+class ListingImage(models.Model):
+
+    def upload_path(instance, filename):
+        return f'listings/accomodation_{instance.listing.accomodationid}/{filename}'
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            super().save(*args, **kwargs)
+
+        if self.image and not self.thumbnail:
+            self.thumbnail = create_thumbnail(self.image)
+            super().save(update_fields=['thumbnail'])
+
+    id = models.AutoField(primary_key=True)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(
+        upload_to=upload_path,
+        validators=[FileExtensionValidator(['jpg', 'png', 'jpeg'])]
+    )
+    thumbnail = models.ImageField(
+        upload_to=upload_path,
+        validators=[FileExtensionValidator(['jpg', 'png', 'jpeg'])],
+        null=True, 
+        blank=True)
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'image_accomodation'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['listing'],
+                condition=Q(is_main=True),
+                name='unique_main_image_per_listing'
+            )
+        ]
--- backend/listings_service/serializers.py
+++ backend/listings_service/serializers.py
@@ -1,11 +1,44 @@
 from rest_framework import serializers
-from .models import Listing
+from .models import Listing, ListingImage
+
+class ListingImageSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+    thumbnail_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ListingImage
+        fields = ['id', 'image_url', 'thumbnail_url', 'is_main']
+
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.image.url)
+
+    def get_thumbnail_url(self, obj):
+        request = self.context.get('request')
+        if obj.thumbnail:
+            return request.build_absolute_uri(obj.thumbnail.url)
+        return None
 
 class ListingSerializer(serializers.ModelSerializer):
 
+    images = ListingImageSerializer(many=True, read_only=True)
     class Meta:
         model = Listing
-        fields = '__all__'
+        fields = [
+            'accomodationid',
+            'owner', # Maybe add an UserSerializer type in this matter
+            'municipality',
+            'title',
+            'description',
+            'bedrooms',
+            'bathrooms',
+            'locationdesc',
+            'addresstext',
+            'propertytype',
+            'pricepernight',
+            'maxguests',
+            'images'
+        ]
         read_only_fields = ['accomodationid']
 
 class PublishListingSerializer(serializers.ModelSerializer):
